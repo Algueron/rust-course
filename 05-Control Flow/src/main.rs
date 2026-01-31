6ce015ef74a277b6c52ea2512665b4d9ddc92652
Julien Papillon <julien.papillon@gmail.com>
2026-01-31 13:44:54 +0100

    feat: Add Control Flow lesson

---
+++ 05-Control Flow/src/main.rs
@@ -0,0 +1,77 @@
+/*
+Define a `color_to_number` function that accepts a 'color'
+parameter (a string). Use if, else if, and else
+statements to return a corresponding numeric value based
+on the following rules:
+1. If the color is "red", return 1.
+2. If the color is "green", return 2.
+3. If the color is "blue", return 3.
+4. If the color is any other string, return 0.
+ 
+Refactor the function above to use the `match` statement
+instead of if, else if, and else.
+ 
+Define a `factorial` function that calculates the
+factorial of a number. The factorial is the product
+of multiplying a number by every incremental
+number leading up to it, starting from 1.
+ 
+Examples:
+The factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120
+factorial(5) should return 120.
+ 
+The factorial of 4 is 4 * 3 * 2 * 1 = 24
+factorial(4) should return 24.
+ 
+Implement two solutions/functions for the problem.
+The first solution should not use recursion.
+The second solution should use recursion.
+*/
+
+fn color_to_number(color: &str) -> u8 {
+    if color == "red" {
+        return 1;
+    } else if color == "green" {
+        return 2;
+    }
+    else if color == "blue" {
+        return 3;
+    } else {
+        return 0;
+    }
+}
+
+fn color_to_number_match(color: &str) -> u8 {
+    match color {
+        "red" => 1,
+        "green" => 2,
+        "blue" => 3,
+        _ => 0
+    }
+}
+
+fn factorial_loop(number: i32) -> i32 {
+    let mut result = 1;
+    let mut current = number;
+
+    while current > 0 {
+        result *= current;
+        current -= 1;
+    }
+    result
+}
+
+fn factorial(number: i32) -> i32 {
+    if number <= 1 {
+        1
+    } else {
+        number * factorial(number - 1)
+    }
+}
+
+fn main() {
+    println!("red:{},blue:{},green:{},yellow:{}", color_to_number("red"), color_to_number("blue"), color_to_number("green"), color_to_number("yellow"));
+    println!("red:{},blue:{},green:{},yellow:{}", color_to_number_match("red"), color_to_number_match("blue"), color_to_number_match("green"), color_to_number_match("yellow"));
+    println!("5!={},4!={}", factorial_loop(5), factorial_loop(4));
+    println!("5!={},4!={}", factorial(5), factorial(4));
+}
